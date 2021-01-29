55037ae46a22cc481b728a883f8d57fed38cd7b7
Rawat, Narendra Singh <narendrasingh.rawat@nobelbiocare.com>
2021-01-29 16:20:00 +0530

    DbUp Migration implemented

---
+++ src/Database.Migration/Program.cs
@@ -0,0 +1,40 @@
+﻿using DbUp;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Database.Migration
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var connectionString = ConnectionStringHelper.GetConnectionString("DbUpTest");
+
+            var upgrader =
+                DeployChanges.To
+                    .SqlDatabase(connectionString)
+                    .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                    .LogToConsole()
+                    .Build();
+
+            var result = upgrader.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+#if DEBUG
+                Console.ReadLine();
+#endif
+                return -1;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!");
+            Console.ResetColor();
+            return 0;
+        }
+    }
+}
